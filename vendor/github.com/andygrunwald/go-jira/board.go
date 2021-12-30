3ad83cf0816bf8df8d04dd3f15a323058664199a
Doroszlai, Attila <adoroszlai@apache.org>
2021-12-30 17:24:34 +0100

    Support Personal Access Token auth in Jira
    A new --token flag is added.  If specified, the password (from config or
    netrc) is treated as a token, and sent to Jira in the `Authorization`
    HTTP header instead of using Basic Auth.
    
    https://confluence.atlassian.com/enterprise/using-personal-access-tokens-1026032365.html

--- vendor/github.com/andygrunwald/go-jira/board.go
+++ vendor/github.com/andygrunwald/go-jira/board.go
@@ -1,13 +1,15 @@
 package jira
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 	"time"
 )
 
-// BoardService handles Agile Boards for the JIRA instance / API.
+// BoardService handles Agile Boards for the Jira instance / API.
 //
-// JIRA API docs: https://docs.atlassian.com/jira-software/REST/server/
+// Jira API docs: https://docs.atlassian.com/jira-software/REST/server/
 type BoardService struct {
 	client *Client
 }
@@ -21,7 +23,7 @@ type BoardsList struct {
 	Values     []Board `json:"values" structs:"values"`
 }
 
-// Board represents a JIRA agile board
+// Board represents a Jira agile board
 type Board struct {
 	ID       int    `json:"id,omitempty" structs:"id,omitempty"`
 	Self     string `json:"self,omitempty" structs:"self,omitempty"`
@@ -34,7 +36,7 @@ type Board struct {
 type BoardListOptions struct {
 	// BoardType filters results to boards of the specified type.
 	// Valid values: scrum, kanban.
-	BoardType string `url:"boardType,omitempty"`
+	BoardType string `url:"type,omitempty"`
 	// Name filters results to boards that match or partially match the specified name.
 	Name string `url:"name,omitempty"`
 	// ProjectKeyOrID filters results to boards that are relevant to a project.
@@ -44,12 +46,24 @@ type BoardListOptions struct {
 	SearchOptions
 }
 
-// Wrapper struct for search result
-type sprintsResult struct {
-	Sprints []Sprint `json:"values" structs:"values"`
+// GetAllSprintsOptions specifies the optional parameters to the BoardService.GetList
+type GetAllSprintsOptions struct {
+	// State filters results to sprints in the specified states, comma-separate list
+	State string `url:"state,omitempty"`
+
+	SearchOptions
 }
 
-// Sprint represents a sprint on JIRA agile board
+// SprintsList reflects a list of agile sprints
+type SprintsList struct {
+	MaxResults int      `json:"maxResults" structs:"maxResults"`
+	StartAt    int      `json:"startAt" structs:"startAt"`
+	Total      int      `json:"total" structs:"total"`
+	IsLast     bool     `json:"isLast" structs:"isLast"`
+	Values     []Sprint `json:"values" structs:"values"`
+}
+
+// Sprint represents a sprint on Jira agile board
 type Sprint struct {
 	ID            int        `json:"id" structs:"id"`
 	Name          string     `json:"name" structs:"name"`
@@ -61,13 +75,66 @@ type Sprint struct {
 	State         string     `json:"state" structs:"state"`
 }
 
-// GetAllBoards will returns all boards. This only includes boards that the user has permission to view.
+// BoardConfiguration represents a boardConfiguration of a jira board
+type BoardConfiguration struct {
+	ID           int                            `json:"id"`
+	Name         string                         `json:"name"`
+	Self         string                         `json:"self"`
+	Location     BoardConfigurationLocation     `json:"location"`
+	Filter       BoardConfigurationFilter       `json:"filter"`
+	SubQuery     BoardConfigurationSubQuery     `json:"subQuery"`
+	ColumnConfig BoardConfigurationColumnConfig `json:"columnConfig"`
+}
+
+// BoardConfigurationFilter reference to the filter used by the given board.
+type BoardConfigurationFilter struct {
+	ID   string `json:"id"`
+	Self string `json:"self"`
+}
+
+// BoardConfigurationSubQuery  (Kanban only) - JQL subquery used by the given board.
+type BoardConfigurationSubQuery struct {
+	Query string `json:"query"`
+}
+
+// BoardConfigurationLocation reference to the container that the board is located in
+type BoardConfigurationLocation struct {
+	Type string `json:"type"`
+	Key  string `json:"key"`
+	ID   string `json:"id"`
+	Self string `json:"self"`
+	Name string `json:"name"`
+}
+
+// BoardConfigurationColumnConfig lists the columns for a given board in the order defined in the column configuration
+// with constrainttype (none, issueCount, issueCountExclSubs)
+type BoardConfigurationColumnConfig struct {
+	Columns        []BoardConfigurationColumn `json:"columns"`
+	ConstraintType string                     `json:"constraintType"`
+}
+
+// BoardConfigurationColumn lists the name of the board with the statuses that maps to a particular column
+type BoardConfigurationColumn struct {
+	Name   string                           `json:"name"`
+	Status []BoardConfigurationColumnStatus `json:"statuses"`
+}
+
+// BoardConfigurationColumnStatus represents a status in the column configuration
+type BoardConfigurationColumnStatus struct {
+	ID   string `json:"id"`
+	Self string `json:"self"`
+}
+
+// GetAllBoardsWithContext will returns all boards. This only includes boards that the user has permission to view.
 //
-// JIRA API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-getAllBoards
-func (s *BoardService) GetAllBoards(opt *BoardListOptions) (*BoardsList, *Response, error) {
+// Jira API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-getAllBoards
+func (s *BoardService) GetAllBoardsWithContext(ctx context.Context, opt *BoardListOptions) (*BoardsList, *Response, error) {
 	apiEndpoint := "rest/agile/1.0/board"
 	url, err := addOptions(apiEndpoint, opt)
-	req, err := s.client.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	req, err := s.client.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -75,19 +142,25 @@ func (s *BoardService) GetAllBoards(opt *BoardListOptions) (*BoardsList, *Respon
 	boards := new(BoardsList)
 	resp, err := s.client.Do(req, boards)
 	if err != nil {
-		return nil, resp, err
+		jerr := NewJiraError(resp, err)
+		return nil, resp, jerr
 	}
 
 	return boards, resp, err
 }
 
-// GetBoard will returns the board for the given boardID.
+// GetAllBoards wraps GetAllBoardsWithContext using the background context.
+func (s *BoardService) GetAllBoards(opt *BoardListOptions) (*BoardsList, *Response, error) {
+	return s.GetAllBoardsWithContext(context.Background(), opt)
+}
+
+// GetBoardWithContext will returns the board for the given boardID.
 // This board will only be returned if the user has permission to view it.
 //
-// JIRA API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-getBoard
-func (s *BoardService) GetBoard(boardID int) (*Board, *Response, error) {
+// Jira API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-getBoard
+func (s *BoardService) GetBoardWithContext(ctx context.Context, boardID int) (*Board, *Response, error) {
 	apiEndpoint := fmt.Sprintf("rest/agile/1.0/board/%v", boardID)
-	req, err := s.client.NewRequest("GET", apiEndpoint, nil)
+	req, err := s.client.NewRequestWithContext(ctx, "GET", apiEndpoint, nil)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -95,22 +168,29 @@ func (s *BoardService) GetBoard(boardID int) (*Board, *Response, error) {
 	board := new(Board)
 	resp, err := s.client.Do(req, board)
 	if err != nil {
-		return nil, resp, err
+		jerr := NewJiraError(resp, err)
+		return nil, resp, jerr
 	}
+
 	return board, resp, nil
 }
 
-// CreateBoard creates a new board. Board name, type and filter Id is required.
+// GetBoard wraps GetBoardWithContext using the background context.
+func (s *BoardService) GetBoard(boardID int) (*Board, *Response, error) {
+	return s.GetBoardWithContext(context.Background(), boardID)
+}
+
+// CreateBoardWithContext creates a new board. Board name, type and filter Id is required.
 // name - Must be less than 255 characters.
 // type - Valid values: scrum, kanban
 // filterId - Id of a filter that the user has permissions to view.
 // Note, if the user does not have the 'Create shared objects' permission and tries to create a shared board, a private
 // board will be created instead (remember that board sharing depends on the filter sharing).
 //
-// JIRA API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-createBoard
-func (s *BoardService) CreateBoard(board *Board) (*Board, *Response, error) {
+// Jira API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-createBoard
+func (s *BoardService) CreateBoardWithContext(ctx context.Context, board *Board) (*Board, *Response, error) {
 	apiEndpoint := "rest/agile/1.0/board"
-	req, err := s.client.NewRequest("POST", apiEndpoint, board)
+	req, err := s.client.NewRequestWithContext(ctx, "POST", apiEndpoint, board)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -118,38 +198,114 @@ func (s *BoardService) CreateBoard(board *Board) (*Board, *Response, error) {
 	responseBoard := new(Board)
 	resp, err := s.client.Do(req, responseBoard)
 	if err != nil {
-		return nil, resp, err
+		jerr := NewJiraError(resp, err)
+		return nil, resp, jerr
 	}
 
 	return responseBoard, resp, nil
 }
 
-// DeleteBoard will delete an agile board.
+// CreateBoard wraps CreateBoardWithContext using the background context.
+func (s *BoardService) CreateBoard(board *Board) (*Board, *Response, error) {
+	return s.CreateBoardWithContext(context.Background(), board)
+}
+
+// DeleteBoardWithContext will delete an agile board.
 //
-// JIRA API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-deleteBoard
-func (s *BoardService) DeleteBoard(boardID int) (*Board, *Response, error) {
+// Jira API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board-deleteBoard
+func (s *BoardService) DeleteBoardWithContext(ctx context.Context, boardID int) (*Board, *Response, error) {
 	apiEndpoint := fmt.Sprintf("rest/agile/1.0/board/%v", boardID)
-	req, err := s.client.NewRequest("DELETE", apiEndpoint, nil)
+	req, err := s.client.NewRequestWithContext(ctx, "DELETE", apiEndpoint, nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	resp, err := s.client.Do(req, nil)
+	if err != nil {
+		err = NewJiraError(resp, err)
+	}
 	return nil, resp, err
 }
 
-// GetAllSprints will returns all sprints from a board, for a given board Id.
+// DeleteBoard wraps DeleteBoardWithContext using the background context.
+func (s *BoardService) DeleteBoard(boardID int) (*Board, *Response, error) {
+	return s.DeleteBoardWithContext(context.Background(), boardID)
+}
+
+// GetAllSprintsWithContext will return all sprints from a board, for a given board Id.
 // This only includes sprints that the user has permission to view.
 //
-// JIRA API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board/{boardId}/sprint
-func (s *BoardService) GetAllSprints(boardID string) ([]Sprint, *Response, error) {
-	apiEndpoint := fmt.Sprintf("rest/agile/1.0/board/%s/sprint", boardID)
-	req, err := s.client.NewRequest("GET", apiEndpoint, nil)
+// Jira API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board/{boardId}/sprint
+func (s *BoardService) GetAllSprintsWithContext(ctx context.Context, boardID string) ([]Sprint, *Response, error) {
+	id, err := strconv.Atoi(boardID)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	result := new(sprintsResult)
+	result, response, err := s.GetAllSprintsWithOptions(id, &GetAllSprintsOptions{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return result.Values, response, nil
+}
+
+// GetAllSprints wraps GetAllSprintsWithContext using the background context.
+func (s *BoardService) GetAllSprints(boardID string) ([]Sprint, *Response, error) {
+	return s.GetAllSprintsWithContext(context.Background(), boardID)
+}
+
+// GetAllSprintsWithOptionsWithContext will return sprints from a board, for a given board Id and filtering options
+// This only includes sprints that the user has permission to view.
+//
+// Jira API docs: https://docs.atlassian.com/jira-software/REST/cloud/#agile/1.0/board/{boardId}/sprint
+func (s *BoardService) GetAllSprintsWithOptionsWithContext(ctx context.Context, boardID int, options *GetAllSprintsOptions) (*SprintsList, *Response, error) {
+	apiEndpoint := fmt.Sprintf("rest/agile/1.0/board/%d/sprint", boardID)
+	url, err := addOptions(apiEndpoint, options)
+	if err != nil {
+		return nil, nil, err
+	}
+	req, err := s.client.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	result := new(SprintsList)
 	resp, err := s.client.Do(req, result)
-	return result.Sprints, resp, err
+	if err != nil {
+		err = NewJiraError(resp, err)
+	}
+
+	return result, resp, err
+}
+
+// GetAllSprintsWithOptions wraps GetAllSprintsWithOptionsWithContext using the background context.
+func (s *BoardService) GetAllSprintsWithOptions(boardID int, options *GetAllSprintsOptions) (*SprintsList, *Response, error) {
+	return s.GetAllSprintsWithOptionsWithContext(context.Background(), boardID, options)
+}
+
+// GetBoardConfigurationWithContext will return a board configuration for a given board Id
+// Jira API docs:https://developer.atlassian.com/cloud/jira/software/rest/#api-rest-agile-1-0-board-boardId-configuration-get
+func (s *BoardService) GetBoardConfigurationWithContext(ctx context.Context, boardID int) (*BoardConfiguration, *Response, error) {
+	apiEndpoint := fmt.Sprintf("rest/agile/1.0/board/%d/configuration", boardID)
+
+	req, err := s.client.NewRequestWithContext(ctx, "GET", apiEndpoint, nil)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	result := new(BoardConfiguration)
+	resp, err := s.client.Do(req, result)
+	if err != nil {
+		err = NewJiraError(resp, err)
+	}
+
+	return result, resp, err
+
+}
+
+// GetBoardConfiguration wraps GetBoardConfigurationWithContext using the background context.
+func (s *BoardService) GetBoardConfiguration(boardID int) (*BoardConfiguration, *Response, error) {
+	return s.GetBoardConfigurationWithContext(context.Background(), boardID)
 }
--- vendor/github.com/andygrunwald/go-jira/project.go
+++ vendor/github.com/andygrunwald/go-jira/project.go
@@ -1,12 +1,15 @@
 package jira
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/google/go-querystring/query"
 )
 
-// ProjectService handles projects for the JIRA instance / API.
+// ProjectService handles projects for the Jira instance / API.
 //
-// JIRA API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project
+// Jira API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project
 type ProjectService struct {
 	client *Client
 }
@@ -21,6 +24,7 @@ type ProjectList []struct {
 	AvatarUrls      AvatarUrls      `json:"avatarUrls" structs:"avatarUrls"`
 	ProjectTypeKey  string          `json:"projectTypeKey" structs:"projectTypeKey"`
 	ProjectCategory ProjectCategory `json:"projectCategory,omitempty" structs:"projectsCategory,omitempty"`
+	IssueTypes      []IssueType     `json:"issueTypes,omitempty" structs:"issueTypes,omitempty"`
 }
 
 // ProjectCategory represents a single project category
@@ -31,38 +35,24 @@ type ProjectCategory struct {
 	Description string `json:"description" structs:"description,omitempty"`
 }
 
-// Project represents a JIRA Project.
+// Project represents a Jira Project.
 type Project struct {
-	Expand       string             `json:"expand,omitempty" structs:"expand,omitempty"`
-	Self         string             `json:"self,omitempty" structs:"self,omitempty"`
-	ID           string             `json:"id,omitempty" structs:"id,omitempty"`
-	Key          string             `json:"key,omitempty" structs:"key,omitempty"`
-	Description  string             `json:"description,omitempty" structs:"description,omitempty"`
-	Lead         User               `json:"lead,omitempty" structs:"lead,omitempty"`
-	Components   []ProjectComponent `json:"components,omitempty" structs:"components,omitempty"`
-	IssueTypes   []IssueType        `json:"issueTypes,omitempty" structs:"issueTypes,omitempty"`
-	URL          string             `json:"url,omitempty" structs:"url,omitempty"`
-	Email        string             `json:"email,omitempty" structs:"email,omitempty"`
-	AssigneeType string             `json:"assigneeType,omitempty" structs:"assigneeType,omitempty"`
-	Versions     []Version          `json:"versions,omitempty" structs:"versions,omitempty"`
-	Name         string             `json:"name,omitempty" structs:"name,omitempty"`
-	Roles        struct {
-		Developers string `json:"Developers,omitempty" structs:"Developers,omitempty"`
-	} `json:"roles,omitempty" structs:"roles,omitempty"`
-	AvatarUrls      AvatarUrls      `json:"avatarUrls,omitempty" structs:"avatarUrls,omitempty"`
-	ProjectCategory ProjectCategory `json:"projectCategory,omitempty" structs:"projectCategory,omitempty"`
-}
-
-// Version represents a single release version of a project
-type Version struct {
-	Self            string `json:"self" structs:"self,omitempty"`
-	ID              string `json:"id" structs:"id,omitempty"`
-	Name            string `json:"name" structs:"name,omitempty"`
-	Archived        bool   `json:"archived" structs:"archived,omitempty"`
-	Released        bool   `json:"released" structs:"released,omitempty"`
-	ReleaseDate     string `json:"releaseDate" structs:"releaseDate,omitempty"`
-	UserReleaseDate string `json:"userReleaseDate" structs:"userReleaseDate,omitempty"`
-	ProjectID       int    `json:"projectId" structs:"projectId,omitempty"` // Unlike other IDs, this is returned as a number
+	Expand          string             `json:"expand,omitempty" structs:"expand,omitempty"`
+	Self            string             `json:"self,omitempty" structs:"self,omitempty"`
+	ID              string             `json:"id,omitempty" structs:"id,omitempty"`
+	Key             string             `json:"key,omitempty" structs:"key,omitempty"`
+	Description     string             `json:"description,omitempty" structs:"description,omitempty"`
+	Lead            User               `json:"lead,omitempty" structs:"lead,omitempty"`
+	Components      []ProjectComponent `json:"components,omitempty" structs:"components,omitempty"`
+	IssueTypes      []IssueType        `json:"issueTypes,omitempty" structs:"issueTypes,omitempty"`
+	URL             string             `json:"url,omitempty" structs:"url,omitempty"`
+	Email           string             `json:"email,omitempty" structs:"email,omitempty"`
+	AssigneeType    string             `json:"assigneeType,omitempty" structs:"assigneeType,omitempty"`
+	Versions        []Version          `json:"versions,omitempty" structs:"versions,omitempty"`
+	Name            string             `json:"name,omitempty" structs:"name,omitempty"`
+	Roles           map[string]string  `json:"roles,omitempty" structs:"roles,omitempty"`
+	AvatarUrls      AvatarUrls         `json:"avatarUrls,omitempty" structs:"avatarUrls,omitempty"`
+	ProjectCategory ProjectCategory    `json:"projectCategory,omitempty" structs:"projectCategory,omitempty"`
 }
 
 // ProjectComponent represents a single component of a project
@@ -81,32 +71,70 @@ type ProjectComponent struct {
 	ProjectID           int    `json:"projectId" structs:"projectId,omitempty"`
 }
 
-// GetList gets all projects form JIRA
+// PermissionScheme represents the permission scheme for the project
+type PermissionScheme struct {
+	Expand      string       `json:"expand" structs:"expand,omitempty"`
+	Self        string       `json:"self" structs:"self,omitempty"`
+	ID          int          `json:"id" structs:"id,omitempty"`
+	Name        string       `json:"name" structs:"name,omitempty"`
+	Description string       `json:"description" structs:"description,omitempty"`
+	Permissions []Permission `json:"permissions" structs:"permissions,omitempty"`
+}
+
+// GetListWithContext gets all projects form Jira
 //
-// JIRA API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project-getAllProjects
+// Jira API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project-getAllProjects
+func (s *ProjectService) GetListWithContext(ctx context.Context) (*ProjectList, *Response, error) {
+	return s.ListWithOptionsWithContext(ctx, &GetQueryOptions{})
+}
+
+// GetList wraps GetListWithContext using the background context.
 func (s *ProjectService) GetList() (*ProjectList, *Response, error) {
+	return s.GetListWithContext(context.Background())
+}
+
+// ListWithOptionsWithContext gets all projects form Jira with optional query params, like &GetQueryOptions{Expand: "issueTypes"} to get
+// a list of all projects and their supported issuetypes
+//
+// Jira API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project-getAllProjects
+func (s *ProjectService) ListWithOptionsWithContext(ctx context.Context, options *GetQueryOptions) (*ProjectList, *Response, error) {
 	apiEndpoint := "rest/api/2/project"
-	req, err := s.client.NewRequest("GET", apiEndpoint, nil)
+	req, err := s.client.NewRequestWithContext(ctx, "GET", apiEndpoint, nil)
 	if err != nil {
 		return nil, nil, err
+	}
+
+	if options != nil {
+		q, err := query.Values(options)
+		if err != nil {
+			return nil, nil, err
+		}
+		req.URL.RawQuery = q.Encode()
 	}
 
 	projectList := new(ProjectList)
 	resp, err := s.client.Do(req, projectList)
 	if err != nil {
-		return nil, resp, err
+		jerr := NewJiraError(resp, err)
+		return nil, resp, jerr
 	}
+
 	return projectList, resp, nil
 }
 
-// Get returns a full representation of the project for the given issue key.
-// JIRA will attempt to identify the project by the projectIdOrKey path parameter.
+// ListWithOptions wraps ListWithOptionsWithContext using the background context.
+func (s *ProjectService) ListWithOptions(options *GetQueryOptions) (*ProjectList, *Response, error) {
+	return s.ListWithOptionsWithContext(context.Background(), options)
+}
+
+// GetWithContext returns a full representation of the project for the given issue key.
+// Jira will attempt to identify the project by the projectIdOrKey path parameter.
 // This can be an project id, or an project key.
 //
-// JIRA API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project-getProject
-func (s *ProjectService) Get(projectID string) (*Project, *Response, error) {
+// Jira API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project-getProject
+func (s *ProjectService) GetWithContext(ctx context.Context, projectID string) (*Project, *Response, error) {
 	apiEndpoint := fmt.Sprintf("rest/api/2/project/%s", projectID)
-	req, err := s.client.NewRequest("GET", apiEndpoint, nil)
+	req, err := s.client.NewRequestWithContext(ctx, "GET", apiEndpoint, nil)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -114,7 +142,41 @@ func (s *ProjectService) Get(projectID string) (*Project, *Response, error) {
 	project := new(Project)
 	resp, err := s.client.Do(req, project)
 	if err != nil {
-		return nil, resp, err
+		jerr := NewJiraError(resp, err)
+		return nil, resp, jerr
 	}
+
 	return project, resp, nil
+}
+
+// Get wraps GetWithContext using the background context.
+func (s *ProjectService) Get(projectID string) (*Project, *Response, error) {
+	return s.GetWithContext(context.Background(), projectID)
+}
+
+// GetPermissionSchemeWithContext returns a full representation of the permission scheme for the project
+// Jira will attempt to identify the project by the projectIdOrKey path parameter.
+// This can be an project id, or an project key.
+//
+// Jira API docs: https://docs.atlassian.com/jira/REST/latest/#api/2/project-getProject
+func (s *ProjectService) GetPermissionSchemeWithContext(ctx context.Context, projectID string) (*PermissionScheme, *Response, error) {
+	apiEndpoint := fmt.Sprintf("/rest/api/2/project/%s/permissionscheme", projectID)
+	req, err := s.client.NewRequestWithContext(ctx, "GET", apiEndpoint, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ps := new(PermissionScheme)
+	resp, err := s.client.Do(req, ps)
+	if err != nil {
+		jerr := NewJiraError(resp, err)
+		return nil, resp, jerr
+	}
+
+	return ps, resp, nil
+}
+
+// GetPermissionScheme wraps GetPermissionSchemeWithContext using the background context.
+func (s *ProjectService) GetPermissionScheme(projectID string) (*PermissionScheme, *Response, error) {
+	return s.GetPermissionSchemeWithContext(context.Background(), projectID)
 }
